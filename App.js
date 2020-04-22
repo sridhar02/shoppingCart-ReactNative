fea844cbdf6f3b4e7bb4251b6a6e03a3d0566738
Sridhar Katta <kattasridhar02@gmail.com>
2020-04-22 12:26:03 +0530

    Header component and imp that in app.js file

--- App.js
+++ App.js
@@ -9,21 +9,19 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
+import Header from './components/Header';
+
 const App = () => {
   return (
-    <>
-      <View style={styles.container}>
-        <Text>Hello world</Text>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Header />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 
