99f12cbf3e8e378a1fda8acb3f90d389dae2746b
Sridhar Katta <kattasridhar02@gmail.com>
2020-04-22 15:47:48 +0530

    components like Header ,ListItem and Additem are written

---
+++ components/ListItem.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
+function ListItem({item, deleteItem}) {
+  return (
+    <TouchableOpacity style={styles.listItem}>
+      <View style={styles.listItemview}>
+        <Text styel={styles.listItemText}>{item.text}</Text>
+        <Icon
+          name="remove"
+          size={20}
+          color="firebrick"
+          onPress={() => deleteItem(item.id)}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  listItemview: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  listItemText: {
+    fontSize: 18,
+  },
+});
+
+export default ListItem;
